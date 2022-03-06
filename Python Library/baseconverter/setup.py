d0527f106931538c7f13e10ba9acb866f0e267a4
LQss11 <49565791+LQss11@users.noreply.github.com>
2022-03-06 23:45:50 +0100

    Add Library metadata

---
+++ Python Library/baseconverter/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pids",
+    version="0.1",
+    description="A tiny Python library for generating public IDs from integers",
+    author="Simon Willison",
+    url="https://github.com/simonw/...",
+    license="Apache License, Version 2.0",    
+    py_modules=["pids"],
+)
